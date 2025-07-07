bcb397996527775ea80690ac7d7d1ae6a04e863d
Pallavi <pallavidule@gmail.com>
2025-07-07 18:42:14 +0200

    filter pokemon by name

--- src/pages/components/PokemonTable.tsx
+++ src/pages/components/PokemonTable.tsx
@@ -1,33 +1,58 @@
 import Link from "next/link"
+import { useRouter } from "next/router";
+import { useState } from "react"
 
-const PokemonTable = ({pokemons, currentPage}: {pokemons: any[], currentPage: number}) => {
-  return (
-    <div>
-        <table className="w-full border-collapse border border-gray-300">
-            <thead className="bg-gray-200">
-            <tr>
-                <th className="border p-2 text-left">Name</th>
-            </tr>
-            </thead>
-            <tbody>
-            {pokemons.map((pokemon) => (
-                <tr key={pokemon.name} className="border hover:bg-gray-100">
-                <td className="p-2 capitalize">{pokemon.name}</td>
+const PokemonTable = ({pokemons, currentPage, isFiltered}: {pokemons: any[], currentPage: number, isFiltered: boolean   }) => {
+    const [searchText, setSearchText] = useState(''); 
+    const router = useRouter();
+
+    const handleSearchByName = (event: React.FormEvent) => {
+        event.preventDefault();
+        if (searchText.trim()) {
+            router.push(`/?name=${searchText.trim().toLowerCase()}`);
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSearchByName}>
+                <input 
+                    type='text'
+                    value={searchText}  
+                    onChange={(event) => setSearchText(event.target.value)}
+                    placeholder="Search Pokémon by name"
+                    className="border p-2 rounded w-64"/>
+                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
+                    Search
+                </button>
+            </form>
+            <table className="w-full border-collapse border border-gray-300">
+                <thead className="bg-gray-200">
+                <tr>
+                    <th className="border p-2 text-left">Name</th>
                 </tr>
-            ))}
-            </tbody>
-        </table>
-        <div className="mt-4 flex gap-2">
-        {currentPage > 1 && (
-          <Link href={`/?page=${currentPage - 1}`}>
-            <button className="px-4 py-2 bg-blue-500 text-white rounded">Previous</button>
-          </Link>
-        )}
-        <Link href={`/?page=${currentPage + 1}`}>
-          <button className="px-4 py-2 bg-blue-500 text-white rounded">Next</button>
-        </Link>
-      </div>
-    </div>
+                </thead>
+                <tbody>
+                {pokemons.map((pokemon) => (
+                    <tr key={pokemon.name} className="border hover:bg-gray-100">
+                    <td className="p-2 capitalize">{pokemon.name}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            {!isFiltered && (
+                <div className="mt-4 flex gap-2">
+                {currentPage > 1 && (
+                <Link href={`/?page=${currentPage - 1}`}>
+                    <button className="px-4 py-2 bg-blue-500 text-white rounded">Previous</button>
+                </Link>
+                )}
+                <Link href={`/?page=${currentPage + 1}`}>
+                <button className="px-4 py-2 bg-blue-500 text-white rounded">Next</button>
+                </Link>
+                </div>
+            )}
+        </div>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,61 @@ import { GetServerSideProps } from "next";
 import PokemonTable from "./components/PokemonTable";
 
 export const getServerSideProps: GetServerSideProps = async(context) => {
-  const page = Number(context.query.page) || 1;
-  const limit = 20;
-  const offset = (page-1)* limit;
-  
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
-  const data = await response.json();
+  const name = (context.query.name as string) || null;
 
-  return {
-    props: {
-      pokemons: data.results || [],
-      page,
+  try {
+    if(name) {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+      
+      if (!response.ok) throw new Error('Not found');
+      
+      const data = await response.json();
+      
+      return {
+        props: {
+          pokemons: [data],
+          page: 1,
+          isFiltered: true,
+          error: null,
+        }
+      }
+    }
+
+    const page = Number(context.query.page) || 1;
+    const limit = 20;
+    const offset = (page-1)* limit;
+    
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+    const data = await response.json();
+
+    return {
+      props: {
+        pokemons: data.results || [],
+        page,
+        isFiltered: false,
+        error: null
+      }
+    }
+  } catch (error){
+    return {
+      props: {
+        pokemons: [],
+        page: 1,
+        isFiltered: false,
+        error: 'Pokemon not found. Please try again.'
+      }
     }
   }
 }
 
 
-export default function Home({pokemons, page}: {pokemons: any[], page: number}) {
+export default function Home({pokemons, page, isFiltered, error}: {pokemons: any[], page: number, isFiltered: boolean, error: string | null}) {
+  console.log('Pokemons:', pokemons);
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Pokémon List (Page {page})</h1>
-      <PokemonTable pokemons={pokemons} currentPage={page} />
+      <PokemonTable pokemons={pokemons} currentPage={page} isFiltered={isFiltered}/>
+      {error && <p className="text-red-500 mt-2">{error}</p>}
     </div>
   );
 }
